a651a6a80be305d9db6c6f4494ceaa0131cc0b44
Michael Cole <michaelcole1991@gmail.com>
2016-11-19 11:51:02 +0000

    MCW Base Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# MCW-Base-Magento-2-Module
+# MCW Base - Magento 2 Module
+Base module used for Magento 2 Modules
+### Version
+1.0.0
---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Register MCW_Base
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MCW_Base',
+    __DIR__
+);
